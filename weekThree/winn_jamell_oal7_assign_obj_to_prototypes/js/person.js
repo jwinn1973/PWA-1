53d14580161f0481d0a02d2deafb51ba8ded5e7b
jwinn1973 <jamellwinn@jamells-MacBook-Pro.local>
2013-12-09 21:22:04 -0500

    update

--- weekThree/winn_jamell_oal7_assign_obj_to_prototypes/js/person.js
+++ weekThree/winn_jamell_oal7_assign_obj_to_prototypes/js/person.js
@@ -10,9 +10,7 @@ window.Person = Person;
 var Person = function(name, action, job, row){
     Person.jobs = ['RingMaster', 'Juggler', 'Clown', 'Usher'];
     Person.actions = ['Practicing', 'Juggling', 'Smiling', 'Talking'];
-
-    this.job = Math.floor(Math.random()*Person.length);
-
-
-    console.log(this.job)
+    
+    //this.job = Math.floor(Math.random()*Person.length);
+    //console.log(this.job)
 };
