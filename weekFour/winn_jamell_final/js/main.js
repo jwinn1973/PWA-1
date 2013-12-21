16ba330d0d0e550a7ad7adb1eed164926db80b23
jwinn1973 <jamellwinn@jamells-MacBook-Pro.local>
2013-12-20 22:54:41 -0500

    final update

--- weekFour/winn_jamell_final/js/main.js
+++ weekFour/winn_jamell_final/js/main.js
@@ -1,6 +1,7 @@
 /*
 PWA-1
 Jamell Winn
+Final
 */
 
 (function(){
@@ -19,17 +20,15 @@ Jamell Winn
         gpa = [2.8, 2.5, 3.9]
     );
 
-
-
-// Console Logs
-// console.log('=================== Student One ===============')
-// console.log('Name: ',studentOne.name);
-// console.log('Address: ', studentOne.address);
-// console.log('City: ', studentOne.city);
-// console.log('State: ',studentOne.state);
-// console.log('GPA: ',studentOne.gpa);
-   studentGpaOne = Student.prototype.calculateGpa(studentOne.gpa);
-   console.log('Student GPA Average: ',studentGpaOne);
+//  Console Logs
+    console.log('=================== Student One ===============')
+    console.log('Name: ',studentOne.name);
+    console.log('Address: ', studentOne.address);
+    console.log('City: ', studentOne.city);
+    console.log('State: ',studentOne.state);
+    console.log('GPA: ',studentOne.gpa);
+    studentGpaOne = Student.prototype.calculateGpa(studentOne.gpa);
+    console.log('Student GPA Average: ',studentGpaOne);
 
     var studentTwo = new Student(
         name = 'Obi Wan',
@@ -39,93 +38,79 @@ Jamell Winn
         gpa = [3.8, 3.9, 3.7]
     );
 // Console Logs
-// console.log('=================== Student Two ===============')
-// console.log('Name: ',studentTwo.name);
-// console.log('Address: ', studentTwo.address);
-// console.log('City: ', studentTwo.city);
-// console.log('State: ',studentTwo.state);
-// console.log('GPA: ',studentTwo.gpa);
+   console.log('=================== Student Two ===============')
+   console.log('Name: ',studentTwo.name);
+   console.log('Address: ', studentTwo.address);
+   console.log('City: ', studentTwo.city);
+   console.log('State: ',studentTwo.state);
+   console.log('GPA: ',studentTwo.gpa);
    studentGpaTwo = Student.prototype.calculateGpa(studentTwo.gpa);
-// console.log('Student GPA Average: ',studentGpaTwo);
+   console.log('Student GPA Average: ',studentGpaTwo);
 
     var studentThree = new Student(
         name = 'Master Yoda',
         address = '30 SimonBock Ave',
         city = 'Wilkes Barre',
         state = 'PA',
-        gpa = [3.9, 4.0, 4.0]
+        gpa = [3.9, 3.8, 3.9]
     );
 // Console Logs
-// console.log('=================== Student Three ===============')
-// console.log('Name: ',studentThree.name);
-// console.log('Address: ', studentThree.address);
-// console.log('City: ', studentThree.city);
-// console.log('State: ',studentThree.state);
-// console.log('GPA: ',studentThree.gpa);
-   studentGpaThree = Student.prototype.calculateGpa(studentThree.gpa);
-// console.log('Student GPA Average: ',studentGpaThree);
+     console.log('=================== Student Three ===============')
+     console.log('Name: ',studentThree.name);
+     console.log('Address: ', studentThree.address);
+     console.log('City: ', studentThree.city);
+     console.log('State: ',studentThree.state);
+     console.log('GPA: ',studentThree.gpa);
+     studentGpaThree = Student.prototype.calculateGpa(studentThree.gpa);
+     console.log('Student GPA Average: ',studentGpaThree);
 
 
     students.push(studentOne, studentTwo, studentThree);
-    studentGpa.push(studentGpaOne, studentGpaTwo, studentGpaThree)
-    //console.log(students.length);
-    var i = 0;
+    studentGpa.push(studentGpaOne, studentGpaTwo, studentGpaThree);
+
+    //console.log(students[2].name);
+
+    var i = -1;
 
     function runProg(){
             i++
-            console.log('running')
-            if(i > students.length){
+            //console.log('running')
+            if(i > students.length-1){
                 endProg();
                 return false;
-            } // end if statment
+            } // end if statement
             else{
-                    getStudent();
-               //;
-               //var address = document.getElementById('address');
-               //address.innerHTML = "Address: " + students[x].address.street + " " + students[x].address.city + ", " + students[x].address.state;
-               //var gpa = document.getElementById('gpa');
-               //gpa.innerHTML = "GPA: " + students[x].gpa[0] + ", " + students[x].gpa[1] + ", " + students[x].gpa[2];
-               //var gpaavg = document.getElementById('gpaavg');
-               //gpaavg.innerHTML = "Avg GPA: " +  Math.round(avggpa*100)/100;
-            //Use Date Method to add date to each student
-               //var d = new Date();
-               //var day = d.getDate();
-               //var month = d.getMonth();
-               //var year = d.getFullYear();
-               //var date = document.getElementById('date');
-               //date.innerHTML = "Date: " + month + "-" + day + "-" + year;
-               //return x;
-            }
-   }; // end runProg function
-
-    function getStudent (){
-        for (var i = 0; i < 0; i++){
                 var name = document.getElementById('name');
                 name.innerHTML = "Name: " + students[i].name;
-                console.log(students[0].name)
-        }
-    }; // end get student
+                var address = document.getElementById('address')
+                address.innerHTML = "Address: " + students[i].address;
+                var gpa = document.getElementById('gpa');
+                gpa.innerHTML = "GPA: " + students[i].gpa;
+                var date = document.getElementById('date');
+                date.innerHTML = "Date: " + getDate();
+                var gpaavg = document.getElementById('gpaavg');
+                gpaavg.innerHTML = "Avg GPA: " + studentGpa[i];
+                //console.log(students[i])
+                } // end else
+ }; // end runProg function
 
+//Use Date Method to add date to each student
+    function getDate(){
+        var d = new Date();
+        var day = d.getDate();
+        var month = d.getMonth();
+        var year = d.getFullYear();
+        date = month + "-" + day + "-" + year;
+        return date
+    }; // end Get Date
+
+// This function will end the prog.
     function endProg(){
-        console.log('ending')
-        var name = document.getElementById('name');
-        name.innerHTML = "Done!   Please Refresh"
         btn.innerHTML = "Done";
         btn.removeEventListener('click', runProg, false)
+
     }// end endProg
 
-
     btn.addEventListener('click',runProg ,false);
-// Click Event for Next Button
-
-    //{
-
-
-
-//    };
-
-
-
 
 })();  // end wrapper
-
